1384bb50d8accf6352f2abf684d5a3af2f003cd1
KesS <barnaul04@mail.ru>
2022-09-02 17:04:41 +0700

    Commit v2

---
+++ peaks/serializer.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+from .models import Pereval, Level, Author,
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('last_name', 'first_name', 'email', )
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    user =UserSerializer(read_only=True)
+    class Meta:
+        model = Author
+        exclude = ('id',)
+
+class LevelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Level
+        # fields='__all__'
+        exclude = ('id',)
+
+class PerevalSerializer(serializers.ModelSerializer):
+    levels = LevelSerializer(read_only=True)
+    user = AuthorSerializer(read_only=True)
+    #levels = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    class Meta:
+        model = Pereval
+        # fields= '__all__'
+        exclude = ('id', 'status')
+
+class PerevalDetailSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Pereval
+        exclude = ('id', )
+
+class PerevalCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pereval
